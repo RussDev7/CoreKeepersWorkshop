4da88c41a245d63038e4afe321a9b78d5b3b3ff8
DannyRuss <33048298+RussDev7@users.noreply.github.com>
2022-12-29 17:41:00 -0800

    Added a custom cursor.

--- src/CoreKeeperInventoryEditor/Properties/Resources.Designer.cs
+++ src/CoreKeeperInventoryEditor/Properties/Resources.Designer.cs
@@ -93,6 +93,16 @@ namespace CoreKeepersWorkshop.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap UICursor {
+            get {
+                object obj = ResourceManager.GetObject("UICursor", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap UnknownItem {
             get {
                 object obj = ResourceManager.GetObject("UnknownItem", resourceCulture);
