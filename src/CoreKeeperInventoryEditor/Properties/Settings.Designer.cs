6b9ddc4d83a068720b8e487dd415ecd109b7235b
DannyRuss <33048298+RussDev7@users.noreply.github.com>
2023-02-17 19:43:26 -0800

    Added quality of life changes.

--- src/CoreKeeperInventoryEditor/Properties/Settings.Designer.cs
+++ src/CoreKeeperInventoryEditor/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace CoreKeepersWorkshop.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.4.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -272,6 +272,18 @@ namespace CoreKeepersWorkshop.Properties {
             }
             set {
                 this["RadialMoveScale"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool TopMost {
+            get {
+                return ((bool)(this["TopMost"]));
+            }
+            set {
+                this["TopMost"] = value;
             }
         }
     }
