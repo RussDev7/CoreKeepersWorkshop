63394a4529a31d73fba5d5200b700662063ac309
DannyRuss <33048298+RussDev7@users.noreply.github.com>
2023-03-17 20:57:33 -0700

    Tweaked a labels text.

--- src/CoreKeeperInventoryEditor/MainForm.Designer.cs
+++ src/CoreKeeperInventoryEditor/MainForm.Designer.cs
@@ -2672,7 +2672,7 @@ namespace CoreKeeperInventoryEditor
             this.label26.Name = "label26";
             this.label26.Size = new System.Drawing.Size(240, 13);
             this.label26.TabIndex = 28;
-            this.label26.Text = "- Scan using raw positions rather then filtered positions.";
+            this.label26.Text = "- Test all positions instead of filtered when scanning.";
             // 
             // checkBox3
             // 
