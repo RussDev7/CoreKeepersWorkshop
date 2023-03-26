89de50153f28f59c2306a5d6d3b9694ffa1236fa
DannyRuss <33048298+RussDev7@users.noreply.github.com>
2023-03-25 19:23:03 -0700

    Bump version.

--- build.bat
+++ build.bat
@@ -6,7 +6,7 @@
 @ECHO OFF
 
 Rem | Set Params
-Set "VersionPrefix=1.3.5.2"
+Set "VersionPrefix=1.3.5.3"
 Set "filename=CoreKeeperInventoryEditor-%VersionPrefix%"
 
 Rem | Install SLN Under x64 Profile
