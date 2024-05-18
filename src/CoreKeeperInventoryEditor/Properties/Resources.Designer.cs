6e7f61d4756695fb791162417e4868301177889d
DannyRuss <33048298+RussDev7@users.noreply.github.com>
2024-05-18 17:28:52 -0500

    Added a visual for where to place torches.

--- src/CoreKeeperInventoryEditor/Properties/Resources.Designer.cs
+++ src/CoreKeeperInventoryEditor/Properties/Resources.Designer.cs
@@ -183,6 +183,16 @@ namespace CoreKeepersWorkshop.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap TorchPlaceholder {
+            get {
+                object obj = ResourceManager.GetObject("TorchPlaceholder", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap UIBackground {
             get {
                 object obj = ResourceManager.GetObject("UIBackground", resourceCulture);
