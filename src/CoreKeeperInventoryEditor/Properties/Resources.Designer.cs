1eb1a179055b949ad467b26beb2666e7b38a3c52
DannyRuss <33048298+RussDev7@users.noreply.github.com>
2025-03-11 15:51:05 -0500

    Updated skill editor with a new skill.

--- src/CoreKeeperInventoryEditor/Properties/Resources.Designer.cs
+++ src/CoreKeeperInventoryEditor/Properties/Resources.Designer.cs
@@ -123,6 +123,16 @@ namespace CoreKeepersWorkshop.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Skill12 {
+            get {
+                object obj = ResourceManager.GetObject("Skill12", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Skill2 {
             get {
                 object obj = ResourceManager.GetObject("Skill2", resourceCulture);
