a7f545d20341c17d3ce113732dfc215dd88f1b94
DannyRuss <33048298+RussDev7@users.noreply.github.com>
2025-09-11 13:16:16 -0500

    Bumped version.

--- src/CoreKeeperInventoryEditor/Properties/AssemblyInfo.cs
+++ src/CoreKeeperInventoryEditor/Properties/AssemblyInfo.cs
@@ -33,4 +33,5 @@ using System.Reflection;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.3.6.6")]
+[assembly: AssemblyFileVersion("1.3.6.7")]
+
